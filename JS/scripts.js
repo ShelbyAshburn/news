5c8cbbb92e515946f5aab045d3a4e2d845213e72
Shelby Ashburn <76406378+ShelbyAshburn@users.noreply.github.com>
2025-06-06 23:35:51 -0400

    Day 1

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+/*!
+* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+*/
+
+/*Clock on the apps.html page*/ 
+function display_c(){
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct()',refresh)
+ }
+ 
+ function display_ct() {
+   var CDate = new Date()
+   var NewDate=CDate.toDateString(); 
+   NewDate = NewDate + " - " + CDate.toLocaleTimeString();
+   document.getElementById('ct').innerHTML = NewDate;
+   display_c();
+ }
